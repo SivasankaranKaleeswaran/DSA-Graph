f94541dd94c97f4d3365fe965fd2b34ffa39b313
Sivasankaran Kaleeswaran <sivasankaran477@gmail.com>
2024-07-06 23:47:36 +0530

    Added link for shortest-path-in-binary-matrix

--- src/main/java/org/example/DijkstrasAlgoPriorityQueue.java
+++ src/main/java/org/example/DijkstrasAlgoPriorityQueue.java
@@ -1,2 +1,92 @@
-package org.example;public class DijkstrasAlgoPriorityQueue {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class DijkstrasAlgoPriorityQueue {
+    public static void main(String[] args) {
+        int V = 3, E=3,S=2;
+        ArrayList<Integer> node1 = new ArrayList<Integer>() {{add(1);add(1);}};
+        ArrayList<Integer> node2 = new ArrayList<Integer>() {{add(2);add(6);}};
+        ArrayList<Integer> node3 = new ArrayList<Integer>() {{add(2);add(3);}};
+        ArrayList<Integer> node4 = new ArrayList<Integer>() {{add(0);add(1);}};
+        ArrayList<Integer> node5 = new ArrayList<Integer>() {{add(1);add(3);}};
+        ArrayList<Integer> node6 = new ArrayList<Integer>() {{add(0);add(6);}};
+
+        ArrayList<ArrayList<Integer>> inter1 = new ArrayList<ArrayList<Integer>>(){
+            {
+                add(node1);
+                add(node2);
+            }
+        };
+        ArrayList<ArrayList<Integer>> inter2 = new ArrayList<ArrayList<Integer>>(){
+            {
+                add(node3);
+                add(node4);
+            }
+        };
+        ArrayList<ArrayList<Integer>> inter3 = new ArrayList<ArrayList<Integer>>(){
+            {
+                add(node5);
+                add(node6);
+            }
+        };
+        ArrayList<ArrayList<ArrayList<Integer>>> adj= new ArrayList<ArrayList<ArrayList<Integer>>>(){
+            {
+                add(inter1); // for 1st node
+                add(inter2); // for 2nd node
+                add(inter3); // for 3rd node
+            }
+        };
+
+        System.out.println(Arrays.toString(dijkstra(V,adj,S)));
+    }
+
+    private static int[] dijkstra(int v, ArrayList<ArrayList<ArrayList<Integer>>> adj, int s) {
+
+        int[] dis = new int[v];
+        PriorityQueue<Djpair> q=new PriorityQueue<Djpair>((x,y)-> x.distance-y.distance);
+
+        for(int i=0;i<v;i++)
+        {
+            dis[i]=Integer.MAX_VALUE;
+        }
+
+        q.add(new Djpair(0,s));
+
+        dis[s]=0;
+        while(!q.isEmpty())
+        {
+            Djpair temp=q.poll();
+            int curr=temp.node;
+            int dist=temp.distance;
+
+            for(int i=0;i<adj.get(curr).size();i++)
+            {
+                int enode=adj.get(curr).get(i).get(0);
+                int edis= adj.get(curr).get(i).get(1);
+
+                if(dist+edis < dis[enode])
+                {
+                    dis[enode]=dist+edis;
+                    q.add(new Djpair(dis[enode], enode));
+                }
+            }
+        }
+
+        return dis;
+    }
+}
+
+class Djpair
+{
+    int node;
+    int distance;
+    Djpair(int distance, int node)
+    {
+        this.distance=distance;
+        this.node=node;
+
+    }
 }
--- src/main/java/org/example/ShortestPathBinaryMatrix.java
+++ src/main/java/org/example/ShortestPathBinaryMatrix.java
@@ -1,2 +1,69 @@
-package org.example;public class ShortestPathBinaryMatrix {
+package org.example;
+
+import java.util.PriorityQueue;
+
+// https://leetcode.com/problems/shortest-path-in-binary-matrix/description/
+public class ShortestPathBinaryMatrix {
+    public static void main(String[] args) {
+        int[][] grid ={{1,0,0},{1,1,0},{1,1,0}};// {{0,1},{1,0}};//{{0,0,0},{1,1,0},{1,1,0}};
+        System.out.println(shortestPathBinaryMatrix(grid));
+
+    }
+
+    public  static int shortestPathBinaryMatrix(int[][] grid) {
+
+        PriorityQueue<Paths> q=new PriorityQueue<>((x,y)-> x.dist-y.dist);
+        int dis[][] = new int[grid.length][grid.length];
+        for(int i=0;i< grid.length;i++)
+        {
+            for(int j=0;j< grid.length;j++)
+            {
+                dis[i][j]=Integer.MAX_VALUE;
+            }
+        }
+        if(grid[0][0]==0)
+        {
+            q.add(new Paths(0,0,1));
+            dis[0][0]=1;
+        }
+
+        while(!q.isEmpty())
+        {
+            Paths t=q.poll();
+            int dist=t.dist;
+            int row=t.row;
+            int col = t.col;
+
+            int[] nr={0,0,-1,1,1,-1,-1,1};
+            int[] nc={-1,1,0,0,1,1,-1,-1};
+
+            for(int i=0;i<8;i++)
+            {
+                int nrow=row+nr[i];
+                int ncol=col+nc[i];
+                if(nrow >-1 && nrow < grid.length && ncol >-1 && ncol< grid.length && grid[nrow][ncol]==0 && dist+1 < dis[nrow][ncol])
+                {
+                    q.add(new Paths(nrow, ncol, dist+1));
+                    dis[nrow][ncol]=dist+1;
+
+                }
+
+            }
+        }
+
+        return dis[grid.length-1][grid.length-1] == Integer.MAX_VALUE? -1: dis[grid.length-1][grid.length-1] ;
+    }
+}
+
+class Paths
+{
+    int row;
+    int col;
+    int dist;
+
+    public Paths(int row, int col, int dist) {
+        this.row = row;
+        this.col = col;
+        this.dist = dist;
+    }
 }
