063b5861cb5aa3dce6c82d2d40336abbc8935bdf
Sivasankaran Kaleeswaran <sivasankaran477@gmail.com>
2024-07-07 23:57:50 +0530

    Added code to find 130. Surrounded Regions

--- src/main/java/org/example/SurroundedRegion.java
+++ src/main/java/org/example/SurroundedRegion.java
@@ -1,2 +1,61 @@
-package org.example;public class SurroundedRegion {
+package org.example;
+
+//https://leetcode.com/problems/surrounded-regions/
+public class SurroundedRegion {
+
+    public static void main(String[] args)
+    {
+        char grid[][] = {
+                {'X', 'X', 'X', 'X'},
+                {'X', 'O', 'X', 'X'},
+                {'X', 'O', 'O', 'X'},
+                {'X', 'O', 'X', 'X'},
+                {'X', 'X', 'O', 'O'}};
+
+        boolean vis[][] = new boolean[grid.length][grid[0].length];
+
+
+        //  int vis[][] = new int[grid.length][grid[0].length];
+        for(int i=0;i< grid[0].length;i++) {
+            dfs(grid, 0, i, vis);
+        }
+        for(int i=0;i< grid[0].length;i++) {
+            dfs(grid, grid.length-1, i, vis);
+        }
+        for(int i=0;i< grid.length;i++) {
+            dfs(grid, i, 0, vis);
+        }
+        for(int i=0;i< grid.length;i++) {
+            dfs(grid, i, grid[0].length-1, vis);
+        }
+
+        int count=0;
+        for(int i=0;i< grid.length;i++)
+        {
+            for(int j=0;j< grid[0].length;j++)
+            {
+                if(grid[i][j]=='X' && vis[])
+                {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+    private static void dfs(char grid[][], int row, int col, boolean vis[][]) {
+
+        if(row>-1 && row< grid.length && col >-1 && col < grid[0].length && grid[row][col]=='O' && !vis[row][col]) {
+            vis[row][col] = true;
+
+            dfs(grid, row + 1, col, vis);
+            dfs(grid, row - 1, col, vis);
+            dfs(grid, row, col - 1, vis);
+            dfs(grid, row, col + 1, vis);
+        }
+        else  return;
+
+
+
+    }
 }
